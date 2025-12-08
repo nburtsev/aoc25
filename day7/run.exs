45a1b337bb1e3be47aa43a57b7599c863b13b1ba
Nikita Burtsev <nikita.burtsev@gmail.com>
2025-12-07 23:05:04 -0500

    day7 p2

--- day7/run.exs
+++ day7/run.exs
@@ -2,50 +2,85 @@ Code.require_file("util.exs")
 
 defmodule Aoc do
   def part1(path) do
-    [start | rest] = Util.read_lines(path)
+    {_, _, edges} = parse_tree(path)
 
-    beams = [start |> String.graphemes() |> Enum.find_index(fn p -> p == "S" end)]
+    edges
+    |> Map.values()
+    |> Enum.flat_map(& &1)
+    |> Enum.uniq()
+    |> length()
+    |> IO.inspect()
+  end
+
+  def part2(path) do
+    {start_col, _, edges} = parse_tree(path)
+
+    count_paths([3, start_col], edges, %{})
+    |> elem(0)
+    |> IO.inspect()
+  end
+
+  def count_paths(node, edges, memo) do
+    if Map.has_key?(memo, node) do
+      {Map.get(memo, node), memo}
+    else
+      children = Map.get(edges, node, [])
+
+      if children == [] do
+        {1, Map.put(memo, node, 1)}
+      else
+        {total, updated_memo} =
+          Enum.reduce(children, {0, memo}, fn child, {count, m} ->
+            {child_count, new_m} = count_paths(child, edges, m)
+            {count + child_count, new_m}
+          end)
+
+        {total, Map.put(updated_memo, node, total)}
+      end
+    end
+  end
+
+  def parse_tree(path) do
+    lines = Util.read_lines(path)
+
+    [first | rest] = lines
+
+    start_col = first |> String.graphemes() |> Enum.find_index(fn p -> p == "S" end)
 
     splitters =
       rest
       |> Enum.map(&String.graphemes/1)
       |> Enum.map(fn line -> Enum.with_index(line) end)
       |> Enum.map(fn line -> Enum.filter(line, fn {p, _i} -> p != "." end) end)
-
-    splitters
-    |> Enum.reduce({beams, 0}, fn line, acc ->
-      {b, c} = acc
-
-      new_beams =
-        Enum.flat_map(b, fn beam ->
-          cond do
-            Enum.any?(line, fn {_, i} -> i == beam end) ->
-              [beam - 1, beam + 1]
-
-            true ->
-              [beam]
-          end
+      |> Enum.with_index(2)
+      |> Enum.filter(fn {line, _} -> length(line) > 0 end)
+      |> Enum.flat_map(fn {line, row} ->
+        Enum.map(line, fn {_, col} ->
+          [row, col]
         end)
-        |> Enum.uniq()
+      end)
 
-      splitters_hit =
-        Enum.map(b, fn beam ->
-          if Enum.any?(line, fn {_, i} -> i == beam end) do
-            1
-          else
-            0
-          end
-        end)
-        |> Enum.sum()
+    edges =
+      splitters
+      |> Enum.reduce(%{}, fn splitter, acc ->
+        left =
+          Enum.filter(splitters, fn [r, c] ->
+            [rs, cs] = splitter
+            c == cs - 1 and r > rs
+          end)
 
-      {new_beams, c + splitters_hit}
-    end)
-    |> elem(1)
-    |> IO.inspect()
-  end
+        right =
+          Enum.filter(splitters, fn [r, c] ->
+            [rs, cs] = splitter
+            c == cs + 1 and r > rs
+          end)
 
-  # brute force did not work, the thing is a graph, need to parse it as a graph and do dfs or something
-  def part2(path) do
+        t = [Enum.at(left, 0), Enum.at(right, 0)]
+
+        Map.put(acc, splitter, t)
+      end)
+
+    {start_col, splitters, edges}
   end
 end
 
@@ -53,6 +88,6 @@ IO.puts("--- Part 1 ---")
 Aoc.part1(Path.join(__DIR__, "input_test.txt"))
 Aoc.part1(Path.join(__DIR__, "input.txt"))
 
-# IO.puts("--- Part 2 ---")
-# Aoc.part2(Path.join(__DIR__, "input_test.txt"))
-# Aoc.part2(Path.join(__DIR__, "input.txt"))
+IO.puts("--- Part 2 ---")
+Aoc.part2(Path.join(__DIR__, "input_test.txt"))
+Aoc.part2(Path.join(__DIR__, "input.txt"))
